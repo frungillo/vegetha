5c03e8059e048008ca2c354bc0b6bd69062d9cd2
frungillo <genny@frungillo.org>
2015-10-22 19:32:44 +0200

    22_10

---
+++ vegethacust/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace vegethacust
+{
+	[Activity (Label = "vegethacust", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			Button btnLeggi = FindViewById<Button> (Resource.Id.btnLeggi);
+			TextView txtMonitor = FindViewById<TextView> (Resource.Id.txtMonitor);
+			button.Click += delegate {
+				customers.CreaMaster();
+				customers c = new customers();
+				c.Cognome = "La Matta";
+				c.Nome = "Sabrina";
+				c.DataIscrizione = new DateTime(2015,10,02);
+				c.Tipo = "T";
+				c.Numero_tessera = 11584;
+				c.Aggiungi();
+
+
+			};
+
+			btnLeggi.Click+= delegate {
+				txtMonitor.Text= "";
+				List<customers> ls = customers.GetTutti();
+				foreach(customers c in ls) {
+					txtMonitor.Append(c.Cognome+" "+c.Nome+ " tipo:"+ c.Tipo+" ("+c.DataIscrizione.ToShortDateString()+")"+"\n");
+				}
+			};
+		}
+	}
+}
+
+
---
+++ vegethacust/customers.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using SQLite;
+using System.IO;
+using System.Collections.Generic;
+
+namespace vegethacust
+{
+	[Table("customers")]
+	public class customers 
+	{
+		string dbPath = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Personal), "cust.db");
+
+		[PrimaryKey,  Column("_numero_tessera")]
+		public int Numero_tessera { get; set;}
+
+		[MaxLength(50)]
+		public string Cognome { get; set;}
+
+		[MaxLength(50)]
+		public string Nome { get; set;}
+
+		[MaxLength(1)]
+		public string Tipo { get; set;}
+
+		public DateTime DataIscrizione { get; set;}
+
+		public customers ()
+		{
+		}
+
+		public static void CreaMaster() {
+			Console.WriteLine ("Creo Database...");
+			string dbPath = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Personal), "cust.db");
+			var db = new SQLiteConnection (dbPath);
+			db.CreateTable<customers> ();
+		
+		}
+
+		public void Aggiorna ()
+		{
+			
+			var db = new SQLiteConnection (dbPath);
+			db.Update (this);
+		}
+
+		public void Aggiungi ()
+		{
+			var db = new SQLiteConnection (dbPath);
+			db.Insert (this);
+		}
+
+		public static void AggiungiTutti(List<customers> c)
+		{
+			
+			string dbPath = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Personal), "cust.db");
+			var db = new SQLiteConnection (dbPath);
+			db.InsertAll (c);
+		}
+
+		public static List<customers> GetTutti() {
+			List<customers> ls = new List<customers>();
+			string dbPath = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Personal), "cust.db");
+			var db = new SQLiteConnection (dbPath);
+			var table = db.Table<customers> ();
+			foreach (customers c in table) {
+				ls.Add(c);
+			}
+			return ls;
+		}
+
+	}
+}
+
